ba5689113c4ea1326ed973bbf907677810027c40
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2019-06-24 15:41:07 +0200

    working version with laravel mysql for circleCi

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -48,7 +48,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' =>  storage_path() . DIRECTORY_SEPARATOR . env('DB_DATABASE','database') . '.sqlite',
             'prefix' => '',
         ],
 
